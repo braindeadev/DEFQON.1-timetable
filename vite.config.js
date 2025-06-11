4b81ec81bb08a032edcc4b5219819bd8ca6dd29b
braindeadev <atte.lindholm@outlook.com>
2025-06-11 17:48:34 +0300

    friday and github pages update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/DEFQON.1-timetable/',
+  base: '/DEFQON.1-timetable/', // ✅ correct for GitHub Pages
   plugins: [react()],
 })
